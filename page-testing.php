4e70871b10a66caa028b658f7455c4d7c8f14f3c
Jo Greybill <jogreybill@gmail.com>
2012-10-23 16:53:26 -0400

    added a testing template for debugging purposes

---
+++ page-testing.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<style type="text/css">
+#header { display: none; }
+</style>
+
+<div style="margin-top: 100px;">
+
+<?=var_dump(fetch_twitter())?>
+
+</div>
+<?php get_footer(); ?>

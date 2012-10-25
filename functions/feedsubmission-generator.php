f324fa1d581271a4163bf9ee6257c00c65856a29
Jo Greybill <jogreybill@gmail.com>
2012-10-25 14:24:46 -0400

    Updated to prod instance

--- functions/feedsubmission-generator.php
+++ functions/feedsubmission-generator.php
@@ -1,7 +1,7 @@
 <?php
 $_SERVER = Array();
 // Needs to match current host
-$_SERVER['HTTP_HOST'] = 'experience.wp3qa.smca.ucf.edu';
+$_SERVER['HTTP_HOST'] = 'experience.cms.smca.ucf.edu';
 require('../../../../wp-load.php');
 print create_feedsubmissions();
 ?>

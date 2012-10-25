7669d750dc64152bc36d526b2c94e728edca2aa1
Jo Greybill <jogreybill@gmail.com>
2012-10-25 14:20:30 -0400

    Added fix for windows tasks to execute file correctly

--- functions/feedsubmission-generator.php
+++ functions/feedsubmission-generator.php
@@ -1,4 +1,7 @@
 <?php
-require('../../../../wp-blog-header.php');
+$_SERVER = Array();
+// Needs to match current host
+$_SERVER['HTTP_HOST'] = 'experience.wp3qa.smca.ucf.edu';
+require('../../../../wp-load.php');
 print create_feedsubmissions();
 ?>
